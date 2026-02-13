f40bf9b68c42a09f917bce4bd0bc1e4cef036f7d
Afnas7 <afnasperingolanasharaf@gmail.com>
2026-02-13 17:21:50 +0530

    hardcode production redirect

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,14 +4,20 @@ import { supabase } from "@/lib/supabaseClient"
 
 export default function Home() {
 
- const loginWithGoogle = async () => {
+const loginWithGoogle = async () => {
+  const redirectUrl =
+    process.env.NODE_ENV === "development"
+      ? "http://localhost:3000/dashboard"
+      : "https://bookmark-f6uchuwqc-afnas-projects-1a8d6b1a.vercel.app/dashboard";
+
   await supabase.auth.signInWithOAuth({
     provider: "google",
     options: {
-      redirectTo: `${window.location.origin}/dashboard`
-    }
+      redirectTo: redirectUrl,
+    },
   });
 };
+
 
 
   return (
